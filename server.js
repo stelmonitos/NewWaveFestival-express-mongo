14947c384d8761cd2bd25683837ee62ae88e8d9f
Jakub Stelmasiak <stelmon3@gmail.com>
2024-05-16 10:11:48 +0200

    last configs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const seatsRoutes = require('./routes/seats.routes.js')
 const concertsRoutes = require('./routes/concerts.routes.js')
@@ -13,10 +14,17 @@ app.use(testimonialsRoutes);
 app.use(concertsRoutes);
 app.use(seatsRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 })
 
-app.listen(4000, () => {
-    console.log('Server is running on port: 4000');
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
 })
