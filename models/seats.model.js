09e53257a3437eb6c22eeb2f9d7636ec4b82cd1e
Jakub Stelmasiak <stelmon3@gmail.com>
2024-06-19 11:52:24 +0200

    add modules

---
+++ models/seats.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const seatsSchema = new mongoose.Schema({
+    day: { type: Number, required: true },
+    seat: { type: Number, required: true },
+    client: { type: String, required: true },
+    email: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Seats', seatsSchema);
--- server.js
+++ server.js
@@ -3,22 +3,32 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/newwavefest', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Database is connected!');
+});
+db.on('error', err => console.log('Error'+ err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port: 8000');
+  console.log('Server is running on port: 8000');
 })
+
 const io = socket(server);
 
 app.use((req, res, next) => {
-    req.io = io;
-    next();
-  });
-  
+  req.io = io;
+  next();
+});
+
 const seatsRoutes = require('./routes/seats.routes.js')
 const concertsRoutes = require('./routes/concerts.routes.js')
 const testimonialsRoutes = require('./routes/testimonials.routes.js')
 app.use(cors({
-    "origin": "*",
+  "origin": "*",
 }));
 
 
@@ -30,22 +40,21 @@ app.use(concertsRoutes);
 app.use(seatsRoutes);
 
 io.on('connection', (socket) => {
-    console.log('New socket!');
-    
-    // Handle disconnection
-    socket.on('disconnect', () => {
-      console.log('disconnected');
-    });
+  console.log('New socket!');
+  
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('disconnected');
   });
+});
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Not found...' });
+  res.status(404).json({ message: 'Not found...' });
 })
-
